f511aa9ab72ab8d5ce1bd7d92a8c0bd663466273
TJ Lane <tjlane@stanford.edu>
2012-11-13 23:18:50 -0800

    testing

--- test/test_xray.py
+++ test/test_xray.py
@@ -7,27 +7,27 @@ from odin import xray
 from odin.testing import skip
 
 import numpy as np
-from numpy.testing import assert_almost_equal, assert_array_almost_equal
+from numpy.testing import assert_almost_equal, assert_array_almost_equal, assert_allclose
 
 class TestBeam():
     
-    def setup():
+    def setup(self):
         
         self.flux = 100.0
         
     
-    def unit_convs():
+    def test_unit_convs(self):
         
         beam = xray.Beam(self.flux, energy=1.0)
         
-        assert_almost_equal(beam.wavelength == 1.2398e4)
-        assert_almost_equal(beam.frequency  == 2.4190e14)
-        assert_almost_equal(beam.wavenumber == (2.0 * np.pi)/1.2398e4)
+        assert_allclose(beam.wavelength, 12.398, rtol=1e-3)
+        assert_allclose(beam.frequency, 2.4190e17, rtol=1e-3)
+        assert_allclose(beam.wavenumber, (2.0 * np.pi)/1.2398e4, rtol=1e-3)
     
         
 class TestDetector():
     
-    def setup():
+    def setup(self):
         self.d = xray.Detector.generic()
         self.spacing=0.05
         self.lim=10.0
@@ -36,16 +36,17 @@ class TestDetector():
         self.l=50.0
     
     @skip
-    def file_loading():
+    def test_file_loading(self):
         
         self.ref_detector = xray.Detector.load('string')
     
-    def recpolar_n_reciprocal():
-        q1 = np.linalg.norm(self.d.reciprocal)
-        q1 = self.recpolar[:,0]
+    def test_recpolar_n_reciprocal(self):
+        q1 = np.sqrt( np.sum( np.power(self.d.reciprocal,2), axis=1) )
+        q2 = self.d.recpolar[:,0]
         assert_array_almost_equal(q1, q2)
-    
-    def recpolar_space():
+   
+    @skip 
+    def test_recpolar_space(self):
         """ try converting backwards """
         
         # this is the "generic" detector in real space
@@ -57,8 +58,7 @@ class TestDetector():
         xyz[:,1] = yy.flatten()
         
         # one slice along the horizontal direction in real space
-        q = self.d.recpolar[:0]
-        h = self.l * np.tan( np.arcsin(q/self.d.k) )
+        q = self.d.reciprocal[:,0]
+        h = self.l * np.tan( 2.0 * np.arcsin(q/self.d.k) )
         assert_array_almost_equal(x, h)
-        
-        
+
