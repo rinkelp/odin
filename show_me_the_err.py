89ab5d600caf3e5d6352799e53deb98fadb469e7
TJ Lane <tjlane@stanford.edu>
2012-11-26 13:51:57 -0800

    ref in

--- show_me_the_err.py
+++ show_me_the_err.py
@@ -1,3 +1,4 @@
+import numpy as np
 from odin import xray
 from mdtraj import trajectory
 
@@ -7,7 +8,6 @@ shot = xray.Shot.simulate(lyz, 1000, d)
 
 intensities = shot.intensities # these are the GPU-generated values
 
-print "Found %d values > 1e6 (abnormally large)" % len(np.where(intensities > 1e6)[0])
-print "Found %d values < 0 (``impossible``)" % len(np.where(intensities > 0.0)[0])
-
+print "Found %d values > 1e7 (abnormally large)" % len(np.where(intensities > 1e7)[0])
+print "%d total intensities" % len(intensities)
 print intensities
