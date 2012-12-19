02e85bf424bc4c58a6f42760dba0681521bd3295
tjlane <thomas.joseph.lane@gmail.com>
2012-12-19 11:26:55 -0600

    added smoke tests for scripts

--- test/test_scripts.py
+++ test/test_scripts.py
@@ -1,18 +1,69 @@
 
-#from odin import scripts
+import os
+import subprocess
+from nose import SkipTest
 
+from mdtraj import trajectory
 
-class TestShoot():
+from odin import xray
+from odin.testing import skip, ref_file
+
+try:
+    from odin import gpuscatter
+    GPU = True
+except ImportError as e:
+    GPU = False
+    
+
+class TestShoot(object):
     
     def setup(self):
-        pass
+        self.file = ref_file('ala2.pdb')
         
     def test_single_gpu(self):
-        pass
+        if not GPU: raise SkipTest
+        cmd = 'shoot -s %s -n 1 -m 512 -o testshot.shot >& /dev/null' % self.file
+        subprocess.check_call(cmd, shell=True)
+        if not os.path.exists('testshot.shot'):
+            raise RuntimeError('no output produced')
+        else:
+            s = xray.Shot.load('testshot.shot')
+            os.remove('testshot.shot')
+            
+    def test_cpu(self):
+        cmd = 'shoot -s %s -n 1 -m 1 -o testshot2.shot >& /dev/null' % self.file
+        subprocess.check_call(cmd, shell=True)
+        if not os.path.exists('testshot2.shot'):
+            raise RuntimeError('no output produced')
+        else:
+            s = xray.Shot.load('testshot2.shot')
+            os.remove('testshot2.shot')
         
-    def test_parallel_gpu(self):
-        pass
         
-    def test_results(self):
-        pass
+def test_plotiq():
+    cmd = 'plotiq -i %s -m 1.0 >& /dev/null' % ref_file('refshot.shot')
+    subprocess.check_call(cmd, shell=True)
+    if not os.path.exists('intensity_plot.pdf'):
+        raise RuntimeError('no output produced')
+    else:
+        os.remove('intensity_plot.pdf')
+        
+        
+def test_plotcorr():
+    cmd = 'plotcorr -i %s >& /dev/null' % ref_file('refshot.shot')
+    subprocess.check_call(cmd, shell=True)
+    if not os.path.exists('correlation_plot.pdf'):
+        raise RuntimeError('no output produced')
+    else:
+        os.remove('correlation_plot.pdf')
+        
+        
+def test_replicate():
+    cmd = 'replicate -i %s -n 10 -d 0.1 >& /dev/null' % ref_file('goldBenchMark.coor')
+    subprocess.check_call(cmd, shell=True)
+    if not os.path.exists('replicated.pdb'):
+        raise RuntimeError('no output produced')
+    else:
+        o = trajectory.load('replicated.pdb')
+        os.remove('replicated.pdb')
         
