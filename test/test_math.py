f56a1abaf87b178f7c731ecb8a4c0322d83e7c54
tjlane <thomas.joseph.lane@gmail.com>
2012-12-26 23:00:49 -0600

    fixed scatter test

--- test/test_math.py
+++ test/test_math.py
@@ -16,26 +16,26 @@ class TestHough(object):
         self.image = plt.imread(ref_file('chough-test.png'))
         self.CM = om.CircularHough()
         
-    @skip
+        
     def test_basic(self):
         # assert that the one circle in test1 gets picked up
-        maxima = self.CM(self.image, radii=20, threshold=1e-4, stencil_width=1,
-                         concentric=False)
+        maxima = self.CM(self.image, concentric=False)
+        print "maxima", maxima
         assert len(maxima) == 1
         assert maxima[0] == (85.0, 155, 143) # confirmed visually
      
-    @skip 
+        
     def test_many_circles(self):
         
         image = plt.imread(ref_file('chough-test2.png'))
         
-        maxima = self.CM(image, radii=20, threshold=1e-4, 
-                         stencil_width=1, concentric=False)
+        maxima = self.CM(image, concentric=False)
                                   
         print "many circles:", maxima
+        assert False
         
-    @skip    
+        
     def test_concentric(self):
-        maxima = self.CM(self.image, radii=20, threshold=1e-4, 
-                         stencil_width=1, concentric=True)    
+        maxima = self.CM(self.image, concentric=True)
+        print "maxima", maxima
         assert maxima == (155, 143)
