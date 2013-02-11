314683c09ed2d9687ed3afd236679ead1f27eacb
tjlane <thomas.joseph.lane@gmail.com>
2013-02-10 17:23:57 -0800

    sorry travis

--- src/python/plot.py
+++ src/python/plot.py
@@ -1,10 +1,18 @@
 
+"""
+A library for plotting pretty images of all kinds.
+"""
+
+import matplotlib.pyplot as plt
 
                                                     
-class img_class (object):
+class InteractiveImshow(object):
     """
-    Imaging class copied from Ingrid Ofte's pyana_misc code                                                     
+    A plotting class 
+    
+    Ingrid Ofte's/pyana_misc                                                     
     """
+    
     def __init__(self, inarr, filename):
         self.inarr = inarr*(inarr>0)
         for i in range(len(inarr)):
@@ -12,6 +20,7 @@ class img_class (object):
         self.filename = filename
         self.cmax = self.inarr.max()
         self.cmin = self.inarr.min()
+        
 
     def on_keypress(self,event):
         if event.key == 'p':
@@ -19,11 +28,12 @@ class img_class (object):
                 os.mkdir(write_dir + runtag)
             pngtag = write_dir + runtag + "/%s.png" % (self.filename)
             print "saving image as " + pngtag
-            P.savefig(pngtag)
+            plt.savefig(pngtag)
         if event.key == 'r':
             colmin, colmax = self.orglims
-            P.clim(colmin, colmax)
-            P.draw()
+            plt.clim(colmin, colmax)
+            plt.draw()
+            
 
     def on_click(self, event):
         if event.inaxes:
@@ -40,16 +50,17 @@ class img_class (object):
             elif event.button is 3 :
                 if value > colmin and value < colmax:
                     colmax = value
-            P.clim(colmin, colmax)
-            P.draw()
+            plt.clim(colmin, colmax)
+            plt.draw()
+            
 
     def draw_img(self):
-        fig = P.figure()
+        fig = plt.figure()
         cid1 = fig.canvas.mpl_connect('key_press_event', self.on_keypress)
         cid2 = fig.canvas.mpl_connect('button_press_event', self.on_click)
         canvas = fig.add_subplot(111)
         canvas.set_title(self.filename)
-        self.axes = P.imshow(self.inarr, vmax = self.cmax, origin='lower')
-        self.colbar = P.colorbar(self.axes, pad=0.01)
+        self.axes = plt.imshow(self.inarr, vmax = self.cmax, origin='lower')
+        self.colbar = plt.colorbar(self.axes, pad=0.01)
         self.orglims = self.axes.get_clim()
-        P.show()
+        plt.show()
