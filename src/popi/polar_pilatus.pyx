1939c9944833529d17b5335bc7fb354ee8e35672
dermen <dermen@stanford.edu>
2013-02-22 11:11:27 -0600

    finished with polarpilatus wrapper

--- src/popi/polar_pilatus.pyx
+++ src/popi/polar_pilatus.pyx
@@ -1,6 +1,8 @@
 import numpy as np
 cimport numpy as np
+import pylab as plt
 
+from odin import parse
   
 cdef extern from "popi.h":
   cdef cppclass PolarPilatus:
@@ -25,13 +27,28 @@ cdef extern from "popi.h":
     void InterpolateToPolar(float qres_, int Nphi_)
 
 
-cdef class polar_pilatus:
+# as a reference see:  http://docs.scipy.org/doc/numpy/user/c-info.how-to-extend.html
+# using code borrowed from the stack overflow:
+#  http://stackoverflow.com/questions/7543675/how-to-convert-pointer-to-c-array-to-python-array
+
+
+# I Modded this and added it to the polaripilatus class: 
+
+np.import_array() # initialize C API to call PyArray_SimpleNewFromData
+#cdef public api tonumpyarray(float * data, int size) with gil:
+# if not (data and size >= 0): raise ValueError
+# cdef np.npy_intp dims = size
+# NOTE: it doesn't take ownership of `data`. You must free `data` yourself
+# return np.PyArray_SimpleNewFromData(1, &dims, np.NPY_FLOAT32, <void*>data)
+
+
+cdef class polarpilatus:
 
   cdef PolarPilatus *pp
 
 #    self.pp has a nice ring to it  ~.~
 
-  def __init__(self, vals, detdist, pixsize,wavelen,a=0,b=0):
+  def __init__(self, cbf_filename,a=0,b=0):
     """
     Converts Pilatus 6M image to polar coordinate image
  
@@ -40,17 +57,29 @@ cdef class polar_pilatus:
     Parameters
     ----------
 
+    cbf_filename : string
+
+    optional:
+    a : float, x center on detector (defaults to Xdim/2)
+    b : float, y center on detector (defaults to Ydim/2)
+
     """
-    X = int(vals.shape[1])
-    Y = int(vals.shape[0])
+    cbf     = parse.CBF(cbf_filename)
+    vals    = cbf.intensities
+    detdist = cbf.path_length
+    wavelen = cbf.wavelength
+    pixsize = cbf.pixel_size[0]
+
+    X = vals.shape[1]
+    Y = vals.shape[0]
 
     if a == 0 and b == 0:
-      a = float(X/2.)
-      b = float(Y/2.)
+      a = X/2.
+      b = Y/2.
     cdef np.ndarray[ndim=1, dtype=np.float32_t] v
     v = np.ascontiguousarray(vals.flatten(), dtype=np.float32)
-    self.pp = new PolarPilatus(X, Y, &v[0], detdist, pixsize,
-			        wavelen, a, b)
+    self.pp = new PolarPilatus(int(X), int(Y), &v[0], float(detdist), float(pixsize),
+			        float(wavelen), float(a), float(b))
   def __dealloc__(self):
     del self.pp
   
@@ -87,11 +116,36 @@ cdef class polar_pilatus:
   #property polar_pixels:
   #  def __get__(self):
   #    return self.polar_pixels[0]
-  
+ 
+  def get_polar_pixels(self):
+    """ returns the polar pixels as 2d numpy array"""
+
+    ## this is the best I could come up with a.t.m. but im not 100% confident in it
+    ## --> just borrowing from the webs.. see the note and refs above
+    
+    cdef np.npy_intp dims = self.pp.Nphi*self.pp.Nq
+    polpix =  np.PyArray_SimpleNewFromData(1, &dims, np.NPY_FLOAT32, <void*> self.pp.polar_pixels)
+    return polpix.reshape( (self.pp.Nq,self.pp.Nphi) )
+    
+    #polpix = np.fromiter(self.pp.polar_pixels,dtype='float32',count=self.pp.Nphi*self.pp.Nq)
+    #print self.pp.Nphi*self.pp.Nq
+    #polpix = tonumpyarray(self.pp.polar_pixels,self.pp.Nphi*self.pp.Nq)
+    #float * data = self.pp.polar_pixels
+ 
   def center(self,qMin,qMax,center_res,Nphi,size):
     """ finds the center of pilatus image"""
     self.pp.Center(float(qMin),float(qMax),float(center_res),int(Nphi), float(size) )
 
   def Interpolate_to_polar(self,qres,Nphi):
     self.pp.InterpolateToPolar(float(qres),int(Nphi))
+
+  def I_profile(self,d):
+    aves = d.mean(axis=1)
+    qvals = [i*self.pp.qres for i in range(len(aves))]
+    plt.plot(qvals,aves,linewidth=2)
+    plt.xlabel(r'$q\,\AA^{-1}$',fontsize=20)
+    plt.ylabel("average intensity",fontsize=20)
+    plt.suptitle("Intensity profile",fontsize=20)
+    plt.show()
+    
 
