88f4df6ad30d48f63bc03e6b34377712282f55f9
tjlane <thomas.joseph.lane@gmail.com>
2013-03-22 12:56:20 -0700

    removed hough tests

--- test/test_math.py
+++ test/test_math.py
@@ -55,6 +55,7 @@ class TestHough(object):
                
         assert_allclose(maxima, ref)
 
+    @skip
     def test_xray_rings(self):
         """ test the Hough transform on some real data """
         # final result confirmed visually
@@ -64,6 +65,7 @@ class TestHough(object):
         CM = om.CircularHough(radii=np.arange(70,95,3))
         center = CM(self.image, mode='concentric')
         
+    @skip
     def test_parallel(self):
         """ test: ensure parallel & serial Hough are consistent """
         parallel_maxima = self.CM(self.image, mode='sharpest')
@@ -71,8 +73,6 @@ class TestHough(object):
         serial_maxima = self.CM(self.image, mode='sharpest')
         assert_allclose(parallel_maxima, serial_maxima)
         
-    def test_limited_convolution(self):
-        pass
         
         
         
