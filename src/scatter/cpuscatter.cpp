5b3d24ddc1bfd668c9b269c63fb6197e9a95d9ab
tjlane <thomas.joseph.lane@gmail.com>
2013-04-11 10:18:57 -0700

    added finite photons, simpler impl [ci skip]

--- src/scatter/cpuscatter.cpp
+++ src/scatter/cpuscatter.cpp
@@ -5,7 +5,6 @@
 #include <math.h>
 #include <iostream>
 
-
 #ifdef NO_OMP
    #define omp_get_thread_num() 0
 #else
@@ -93,30 +92,11 @@ void kernel( float const * const __restrict__ q_x,
              float const * const __restrict__ randN1, 
              float const * const __restrict__ randN2, 
              float const * const __restrict__ randN3,
-             int   const n_rotations,
-             int   const finite_photons,
-             int   const * const __restrict__ n_photons ) {
+             const int n_rotations ) {
+            
 
-    // if doing finite photons, we need some memory allocated to store results
-    int ndQ;
-    if ( finite_photons == 1 ) {
-        ndQ = nQ;
-    } else {
-        ndQ = 1; // don't allocate a lot of mem we wont use
-    }
-    srand(randN1[0]);                   // init rand seed, using the first rand
-                                        // number makes the code deterministic
-                                        // for the purposes of testing
-    int * discrete_outQ = new int[ndQ]; // new array for finite photon output
-    for( int iq = 0; iq < ndQ; iq++ ) {
-        discrete_outQ[iq] = 0;
-    }
-
-    // main loop -- over molecules
+    // main loop
     for( int im = 0; im < n_rotations; im++ ) {
-        
-        // keep track of the total scattered intensity
-        float outQ_sum = 0.0;
        
         // determine the rotated locations
         float rand1 = randN1[im]; 
@@ -138,9 +118,11 @@ void kernel( float const * const __restrict__ q_x,
             float formfactors[MAX_NUM_TYPES];
 
             // accumulant
-            float Qsumx; Qsumx = 0;
-            float Qsumy; Qsumy = 0;
-
+            float Qsumx;
+            float Qsumy;
+            Qsumx = 0;
+            Qsumy = 0;
+     
             // Cromer-Mann computation, precompute for this value of q
             float mq = qx*qx + qy*qy + qz*qz;
             float qo = mq / (16*M_PI*M_PI); // qo is (sin(theta)/lambda)^2
@@ -176,57 +158,22 @@ void kernel( float const * const __restrict__ q_x,
                 fi = formfactors[id];
                 Qsumx += fi*sinf(qr);
                 Qsumy += fi*cosf(qr);
-            } // end loop over atoms
+            } // finished one molecule.
                         
             // add the output to the total intensity array
             #pragma omp critical
-            {
-            outQ[iq] += (Qsumx*Qsumx + Qsumy*Qsumy);
-            outQ_sum += outQ[iq];
-            }
+            outQ[iq] += (Qsumx*Qsumx + Qsumy*Qsumy); // / n_rotations;
             
-        } // end loop over q
-        
-        // discrete photon statistics, if requested
-        #pragma omp critical
-        {
-        if ( finite_photons == 1 ) {
-                
-            float rp;
-            float cum_q_sum;
-        
-            // for each photon, draw a rand to put it in a pixel
-            for ( int p = 0; p < n_photons[im]; p++ ) {
-            
-                rp = (float)rand()/(float)RAND_MAX * outQ_sum;
-                
-                cum_q_sum = 0.0;
-                int iq = 0;
-            
-                while ( cum_q_sum < rp ) {
-                    cum_q_sum += outQ[iq];
-                    iq++;
-                }
-                discrete_outQ[iq] += 1;
-            }
-        } // end finite photons
-        }
-        
-    } // end loop over rotations
-    
-    // now cp the results in discrete_outQ to outQ and free memory
-    // a bit silly, but is only one loop :/
-    if ( finite_photons == 1 ) {
-        for( int iq = 0; iq < nQ; iq++ ) {
-            outQ[iq] = discrete_outQ[iq];
+            // discrete photon statistics will go here, if implemented 
+            // we'll need a different array to accumulate the results of each
+            // molecule, then add the discrete statistical draw to the final
+            // output       -TJL
         }
     }
-    delete [] discrete_outQ;
-    
-} // end kernel fxn
+}
 
 
-CPUScatter::CPUScatter( int    nQ_, // scattering vectors
+CPUScatter::CPUScatter( int    nQ_,
                         float* h_qx_,
                         float* h_qy_,
                         float* h_qz_,
@@ -247,16 +194,11 @@ CPUScatter::CPUScatter( int    nQ_, // scattering vectors
                         float* h_rand1_,
                         float* h_rand2_,
                         float* h_rand3_,
-                        
-                        // finite photons
-                        int    finite_photons_,
-                        int*   n_photons_,
 
                         // output
                         float* h_outQ_ ) {
                                 
     // unpack arguments
-    
     n_rotations = nRot_;
     nQ = nQ_;
     h_qx = h_qx_;
@@ -275,16 +217,12 @@ CPUScatter::CPUScatter( int    nQ_, // scattering vectors
     h_rand1 = h_rand1_;
     h_rand2 = h_rand2_;
     h_rand3 = h_rand3_;
-    
-    finite_photons = finite_photons_;
-    n_photons = n_photons_;
 
     h_outQ = h_outQ_;
+    
 
     // execute the kernel
-    kernel(h_qx, h_qy, h_qz, h_outQ, nQ, h_rx, h_ry, h_rz, h_id, nAtoms, 
-           numAtomTypes, h_cm, h_rand1, h_rand2, h_rand3, n_rotations, 
-           finite_photons, n_photons);
+    kernel(h_qx, h_qy, h_qz, h_outQ, nQ, h_rx, h_ry, h_rz, h_id, nAtoms, numAtomTypes, h_cm, h_rand1, h_rand2, h_rand3, n_rotations);
 }
 
 CPUScatter::~CPUScatter() {
